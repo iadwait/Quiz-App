58131bde5ce22bfe2ce6c6167d9ae3f3b1bc5d63
iadwait <adwaitbarkale@gmail.com>
2021-07-14 21:13:07 +0530

    Added Questions and Worked on UI

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+const quizData = [
+    {
+        questions: "What is the Color of Tim Cook's hair",
+        a: "White",
+        b: "Black",
+        c: "Red",
+        d: "Brown",
+        correct: 'a'
+    },
+    {
+        questions: "What is Nasik Located",
+        a: "Pakistan",
+        b: "India",
+        c: "USA",
+        d: "Germany",
+        correct: 'b'
+    },
+    {
+        questions: "Which is the Most used Programming language",
+        a: "JS",
+        b: "Python",
+        c: "JAVA",
+        d: "Kotlin",
+        correct: 'c'
+    }
+
+]
